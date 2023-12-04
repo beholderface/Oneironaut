ad0c7b2bcf79e568bbde846e284d2c0fd23c6a89
beholderface <arthur@thibault.org>
2023-12-04 13:30:09 -0500

    add worldborder check, a few comments

--- common/src/main/java/net/oneironaut/casting/patterns/spells/great/OpDimTeleport.kt
+++ common/src/main/java/net/oneironaut/casting/patterns/spells/great/OpDimTeleport.kt
@@ -39,6 +39,7 @@ class OpDimTeleport : SpellAction {
         } else {
             val destination = args.getDimIota(0, argc)
             val dimKey = destination.serialize().downcast(NbtCompound.TYPE).getString("dim_key")
+            //iterate over all the worlds to find the desired one
             target.server.worlds.forEach {
                 if (it.registryKey.value.toString() == dimKey){
                     world = it;
@@ -46,44 +47,56 @@ class OpDimTeleport : SpellAction {
                 }
             }
         }
-
-
-        /*if (!HexConfig.server().canTeleportInThisDimension(ctx.world.registryKey))
-            throw MishapLocationTooFarAway(coords, "bad_dimension")*/
+        //do not do the bad thing
         if (!HexConfig.server().canTeleportInThisDimension(worldKey))
             throw MishapLocationTooFarAway(coords, "bad_dimension")
+
         return if (origin == world && !noosphere){
             Triple(
-                Spell(target, origin, world, coords, target.yaw, target.pitch, false),
-                0 * MediaConstants.CRYSTAL_UNIT,
+                Spell(target, origin, world, coords, false),
+                //don't consume amethyst if trying to teleport to the same dimension you're already in
+                0,
                 listOf(ParticleSpray.cloud(target.pos, 2.0))
             )
         } else {
             Triple(
-                Spell(target, origin, world, coords, target.yaw, target.pitch, noosphere),
+                Spell(target, origin, world, coords, noosphere),
                 20 * MediaConstants.CRYSTAL_UNIT,
                 listOf(ParticleSpray.cloud(target.pos, 2.0))
             )
         }
     }
 
-    private data class Spell(val target: ServerPlayerEntity, val origin : ServerWorld, val destination : ServerWorld, val coords : Vec3d, val yaw : Float, val pitch : Float, val noosphere : Boolean) : RenderedSpell {
+    private data class Spell(val target: ServerPlayerEntity, val origin : ServerWorld, val destination : ServerWorld, val coords : Vec3d, val noosphere : Boolean) : RenderedSpell {
         override fun cast(ctx: CastingContext) {
             var x = coords.x
             var y = coords.y
             var z = coords.z
+            val border = destination.worldBorder
             val compressionFactor = origin.dimension.coordinateScale / destination.dimension.coordinateScale
             x *= compressionFactor
             z *= compressionFactor
+            //make sure you don't end up under the nether or something
             if (destination.bottomY > coords.y - 5.0){
                 y = ((destination.bottomY + 5).toDouble())
+            }
+            //make sure you don't end up outside the world border
+            if (x > border.boundEast){
+                x = border.boundEast - 2
+            } else if (x < border.boundWest){
+                x = border.boundWest + 2
+            }
+            if (z > border.boundSouth){
+                z = border.boundSouth - 2
+            } else if (z < border.boundNorth){
+                z = border.boundNorth + 2
             }
             if (noosphere) {
                 ctx.caster.sendMessage(Text.translatable("hexcasting.spell.oneironaut:dimteleport.comingsoon"));
             } else if (origin == destination){
                 ctx.caster.sendMessage(Text.translatable("hexcasting.spell.oneironaut:dimteleport.samedim"));
             }else {
-                target.teleport(destination, x, y, z, 0.0F, 0.0F)
+                target.teleport(destination, x, y, z, target.yaw, target.pitch)
                 target.addStatusEffect(StatusEffectInstance(StatusEffects.SLOW_FALLING, 1200))
             }
             //ctx.caster.sendMessage(Text.of(world.bottomY.toString()));
