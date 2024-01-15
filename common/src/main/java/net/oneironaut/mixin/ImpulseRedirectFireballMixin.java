df28eb7b3f13cb750738e6b48d0c1bd2d1d7e009
beholderface <arthur@thibault.org>
2024-01-15 16:52:16 -0500

    impulse now changes fireball owner

--- common/src/main/java/net/oneironaut/mixin/ImpulseRedirectFireballMixin.java
+++ common/src/main/java/net/oneironaut/mixin/ImpulseRedirectFireballMixin.java
@@ -8,7 +8,10 @@ import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.projectile.ExplosiveProjectileEntity;
 import net.minecraft.entity.projectile.WitherSkullEntity;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Vec3d;
+import net.oneironaut.MiscAPIKt;
+import net.oneironaut.Oneironaut;
 import net.oneironaut.OneironautConfig;
 import net.oneironaut.casting.FireballUpdatePacket;
 import org.spongepowered.asm.mixin.Final;
@@ -32,20 +35,25 @@ public abstract class ImpulseRedirectFireballMixin {
     @Shadow
     private Entity target;
 
+    @Unique
+    private static final Identifier immuneTag = new Identifier(Oneironaut.MOD_ID, "impulse_redirect_blacklist");
     @Inject(method = "cast", at = @At(value = "RETURN", remap = false), remap = false)
     public void redirectFireball(CastingContext ctx, CallbackInfo ci/*, @Local(ordinal = 0) Entity target*/){
         if (target instanceof ExplosiveProjectileEntity explosive && OneironautConfig.getServer().getImpulseRedirectsFireball()){
             /*TrackedData<Float> POWER_X = DataTracker.registerData(ExplosiveProjectileEntity.class, TrackedDataHandlerRegistry.FLOAT);
             TrackedData<Float> POWER_Y = DataTracker.registerData(ExplosiveProjectileEntity.class, TrackedDataHandlerRegistry.FLOAT);
             TrackedData<Float> POWER_Z = DataTracker.registerData(ExplosiveProjectileEntity.class, TrackedDataHandlerRegistry.FLOAT);*/
-            double deltaDelta = 1.0;
+            boolean immune = false;
             if (explosive instanceof WitherSkullEntity skull){
                 //blue skulls are immune to the redirection
                 if (skull.isCharged()){
-                    deltaDelta = 0.0;
+                    immune = true;
                 }
             }
-
+            if (target.getType().isIn(MiscAPIKt.getEntityTagKey(immuneTag))){
+                immune = true;
+            }
+            double deltaDelta = immune ? 0 : 1;
             double deltaX = motion.getX() * deltaDelta;
             double deltaY = motion.getY() * deltaDelta;
             double deltaZ = motion.getZ() * deltaDelta;
@@ -57,10 +65,13 @@ public abstract class ImpulseRedirectFireballMixin {
             explosive.powerX = explosive.powerX + deltaX;
             explosive.powerY = explosive.powerY + deltaY;
             explosive.powerZ = explosive.powerZ + deltaZ;
+            Vec3d newPower = new Vec3d(explosive.powerX, explosive.powerY, explosive.powerZ);
+            if (!immune){
+                explosive.setOwner(ctx.getCaster());
+            }
             /*tracker.set(POWER_X, (float) (explosive.powerX + deltaX));
             tracker.set(POWER_Y, (float) (explosive.powerX + deltaY));
             tracker.set(POWER_Z, (float) (explosive.powerX + deltaZ));*/
-            Vec3d newPower = new Vec3d(explosive.powerX, explosive.powerY, explosive.powerZ);
             if (!newPower.equals(oldPower)){
                 IXplatAbstractions.INSTANCE.sendPacketNear(explosive.getPos(), 128, ctx.getWorld(), new FireballUpdatePacket(newPower, explosive));
             }
