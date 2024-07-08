6d189870e76ee198c7153dcab4c2a23b0bfe5303
beholderface <arthur@thibault.org>
2024-07-08 16:06:15 -0400

    Loot tables in towers should work now
    Also made noosphere structures slightly more common, and budding pseudoamethyst no longer extends `BuddingAmethystBlock`

--- common/src/main/java/net/beholderface/oneironaut/block/SuperBuddingBlock.java
+++ common/src/main/java/net/beholderface/oneironaut/block/SuperBuddingBlock.java
@@ -1,18 +1,17 @@
 package net.beholderface.oneironaut.block;
 
-import at.petrak.hexcasting.common.particles.ConjureParticleOptions;
-import net.minecraft.block.*;
+import net.minecraft.block.AmethystClusterBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.random.Random;
 
-import static java.lang.Math.pow;
-
-public class SuperBuddingBlock extends BuddingAmethystBlock {
+public class SuperBuddingBlock extends Block /*extends BuddingAmethystBlock*/ {
     public SuperBuddingBlock(Settings settings){
         super(settings
                 .ticksRandomly()
